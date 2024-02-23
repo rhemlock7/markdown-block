2736cb7c20257d323c9af8d92b17087359aba125
Ryan Hemlock <ryanhemlock@Ryans-Laptop.local>
2024-02-23 08:48:12 -0500

    Added functionality. Site working as expected for the most part

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,28 @@
+const scriptContainer = document.getElementById('final-script-container');
+const inputForm = document.getElementById('input-form');
+const inputTextArea = document.getElementById('input-text-area');
+const copyButton = document.getElementById('copy-button')
+
+
+function formatMarkdown(event) {
+    event.preventDefault()
+    const inputValue = inputTextArea.value
+    const formattedScript = inputValue.replace(/(\s+-\s+)|[^a-zA-Z.,\/!'...()?]|[\r\n]+/g, ' ')
+
+
+    console.log(formattedScript.trim())
+    scriptContainer.textContent = formattedScript.trim();
+}
+
+inputForm.addEventListener('submit', formatMarkdown)
+
+copyButton.addEventListener('click', function() {
+    scriptContainer.select();
+    try {
+        const successful = document.execCommand('copy'); // Copy the selected content to the clipboard
+        const message = successful ? 'Text copied to clipboard' : 'Unable to copy text to clipboard';
+        console.log(message);
+    } catch (err) {
+        console.error('Error copying text to clipboard:', err);
+    }
+});
